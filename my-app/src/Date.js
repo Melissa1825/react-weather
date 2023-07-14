9b6f12b1d09fdcd458d349e5027bb961bae8f47d
Melissa1825 <114811994+Melissa1825@users.noreply.github.com>
2023-07-14 07:00:11 -0400

    Created weather app

---
+++ my-app/src/Date.js
@@ -0,0 +1,21 @@
+export default function FormatDate(props) {
+    let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let weekDay = weekDays[props.date.getDay()];
+
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    let month = months[props.date.getMonth()];
+
+    let date = props.date.getDate();
+
+    let hours = props.date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+
+    let min = props.date.getMinutes();
+    if (min < 10) {
+        min = `0${min}`;
+    }
+
+    return <div>{weekDay}, {month} {date}  {hours}:{min}</div>
+}
---
+++ my-app/src/WeatherMain.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import "./index.css";
+
+import WeatherData from './WeatherData';
+
+import axios from 'axios';
+
+export default function WeatherMain(props) {
+  const [weather, setWeather] = useState({loaded: false});
+
+  function handleResponse(response) {
+    console.log(response.data);
+
+    setWeather({
+      loaded: true,
+      date: new Date(response.data.dt * 1000), 
+      city: response.data.name,
+      temp: response.data.main.temp,
+      desc: response.data.weather[0].description,
+      feel: response.data.main.feels_like,
+      wind: response.data.wind.speed,
+      humid: response.data.main.humidity,
+    });
+  }
+
+  if (weather.loaded) {
+    return (
+      
+      <div className="row">
+        <div className="col-4">
+          <form id="citySearch">
+            <input id="cityInput" type="search" placeholder="Enter a city" />
+            <input type="submit" value="search" className="button" />
+          </form>
+        </div>  
+
+        <WeatherData data = {weather} />
+    
+      </div>
+    );
+
+  } else {
+    let city = "Toronto";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a0776b8bb0b1556bee1e5658c3acf0c4&units=metric`;
+    axios.get(url).then(handleResponse)
+
+    return "Loading..."
+
+  }
+}
